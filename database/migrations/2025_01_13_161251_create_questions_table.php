2d54b3c40f110e717426d2175a2bbc3d5245d2fa
Christian <chrisdaq@gmail.com>
2025-01-13 12:39:01 -0400

    add missing columns for migrations

--- database/migrations/2025_01_13_161251_create_questions_table.php
+++ database/migrations/2025_01_13_161251_create_questions_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->longText('body');
             $table->integer('viewCount')->default(0);
             $table->integer('score')->default(0);
+            $table->json('tags')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
