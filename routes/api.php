8f41d0b86dabdbee9fb2a87fce13936432593db5
Christian <chrisdaq@gmail.com>
2025-01-18 10:37:21 -0400

    define question routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\QuestionController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Route;
@@ -15,9 +16,23 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('update/profile', 'updateUserInfo');
         Route::put('update/password', 'updateUserPassword');
     });
+    Route::controller(QuestionController::class)->group(function () {
+        Route::get('user/questions', 'authUserQuestion');
+        Route::post('question/store', 'store');
+        Route::put('update/{question}/question', 'update');
+        Route::delete('deletequestion}/question', 'destroy');
+        Route::put('vote/{question}/{type}/question', 'vote');
+        //Route::put('votedown/{question}/{type}/question', 'vote');
+    });
 });
 
 Route::controller(UserController::class)->group(function () {
     Route::post('user/register', 'store')->name('users.register');
     Route::post('user/login', 'auth');
+});
+Route::controller(QuestionController::class)->group(function () {
+    Route::get('questions', 'index');
+    Route::get('question/{question}/show', 'show');
+    Route::get('tag/{tag}/questions', 'questionByTag');
+    Route::get('user/questions', 'questionsByUser');
 });
